6c7406968e0e984ed21d9f6efe125293701b85c6
Till Klocke <github@goesberserk.de>
2011-04-05 21:56:35 +0200

    Extended the macro interface to give the possibility to set properties

--- src/de/akuz/osynce/macro/interfaces/Macro.java
+++ src/de/akuz/osynce/macro/interfaces/Macro.java
@@ -1,6 +1,7 @@
 package de.akuz.osynce.macro.interfaces;
 
 import java.util.List;
+import java.util.Map;
 
 import de.akuz.osynce.macro.CommunicationException;
 
@@ -24,7 +25,7 @@ public interface Macro {
 	 * Configure the device.
 	 * @throws CommunicationException in case an exception occurs
 	 */
-	public void setPersonalData() throws CommunicationException;
+	public void setPersonalData(PersonalData data) throws CommunicationException;
 	
 	/**
 	 * Wipe all stored trainings from the device
@@ -32,5 +33,13 @@ public interface Macro {
 	 * @throws CommunicationException in case an exception occurs
 	 */
 	public boolean erase() throws CommunicationException;
+	
+	/**
+	 * Since we can't enforce a constructor via interface, we offer here
+	 * a method to give implementing classes some properties to. See
+	 * the implementing classes what properties they do expect
+	 * @param properties
+	 */
+	public void init(Map<String, String> properties);
 
 }
