2ded56a66a1dfed7fc9652f5b041ae03e8f01486
Till Klocke <github@goesberserk.de>
2011-04-13 10:13:24 +0200

    Extended ProviderManagerTestCase to increase coverage

--- test/de/akuz/osynce/macro/serial/packet/test/ProviderManagerTestCase.java
+++ test/de/akuz/osynce/macro/serial/packet/test/ProviderManagerTestCase.java
@@ -1,7 +1,7 @@
 package de.akuz.osynce.macro.serial.packet.test;
 
 
-import junit.framework.Assert;
+import org.junit.Assert;
 
 import org.junit.After;
 import org.junit.Before;
@@ -15,11 +15,14 @@ import de.akuz.osynce.macro.serial.packet.NumberOfTrainingsPacket;
 import de.akuz.osynce.macro.serial.packet.NumberOfTrainingsProvider;
 import de.akuz.osynce.macro.serial.packet.ProviderManager;
 import de.akuz.osynce.macro.serial.packet.ProviderManager.PacketProvider;
+import de.akuz.osynce.macro.serial.packet.TrainingDetailPacket;
 import de.akuz.osynce.macro.serial.packet.TrainingDetailProvider;
 
 public class ProviderManagerTestCase {
 	
 	private ProviderManager pm;
+	
+	private byte[] rawData = RawData.trainingDetail0;
 
 	@Before
 	public void setUp() throws Exception {
@@ -47,7 +50,7 @@ public class ProviderManagerTestCase {
 	}
 	
 	@Test
-	public void getEmptyPacket(){
+	public void testGetEmptyPacket(){
 		Packet p = pm.getEmptyPacket((byte)0x00);
 		Assert.assertNotNull(p);
 		Assert.assertTrue(p instanceof GenericPacket);
@@ -55,6 +58,14 @@ public class ProviderManagerTestCase {
 		p = pm.getEmptyPacket(Commands.NUMBER_OF_TRAININGS.toByte());
 		Assert.assertNotNull(p);
 		Assert.assertTrue(p instanceof NumberOfTrainingsPacket);
+	}
+	
+	@Test
+	public void testParsePacket(){
+		Packet packet = pm.parsePacket(rawData);
+		Assert.assertTrue(packet instanceof TrainingDetailPacket);
+		Assert.assertTrue(packet.check());
+		Assert.assertArrayEquals(rawData, packet.getBytes());
 	}
 
 }
