7cf71338545cd89a4ac54ec55e238a293c865517
Till Klocke <github@goesberserk.de>
2011-04-13 10:16:47 +0200

    Removed unused import

--- src/de/akuz/osynce/macro/serial/payloads/TrainingsList.java
+++ src/de/akuz/osynce/macro/serial/payloads/TrainingsList.java
@@ -1,7 +1,6 @@
 package de.akuz.osynce.macro.serial.payloads;
 
 import java.text.ParseException;
-import java.text.SimpleDateFormat;
 import java.util.Collections;
 import java.util.Date;
 import java.util.LinkedList;
